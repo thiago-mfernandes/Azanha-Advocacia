443df6cd56f00bf4f73f6f14efee337a2d74da93
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-06-05 09:56:29 -0300

    ajustes finais

--- azanha-advocacia/js/contact.js
+++ azanha-advocacia/js/contact.js
@@ -35,23 +35,23 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Insira seu nome",
+                    minlength: "Seu nome precisa ter no mínimo 2 caracteres"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Insira o assunto da sua dúvida",
+                    minlength: "O campor assunto precisa ter no mínimo 5 caracteres"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
+                    required: "Insira seu numero de telefone",
+                    minlength: "Seu numero precisa ter no mínimo 8 caracteres"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Insira seu email"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Escreva sua mensagem",
+                    minlength: "Isso é tudo? Tem Certeza?"
                 }
             },
             submitHandler: function(form) {
